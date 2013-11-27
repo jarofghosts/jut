48474d43c28893ada0a70f49675b50deaed6991b
jesse keane <me@jessekeane.me>
2013-11-26 23:35:28 -0800

    wee kinda working

--- bin/jut.js
+++ bin/jut.js
@@ -1,5 +1,6 @@
 var nopt = require('nopt'),
     jut = require('../'),
+    fs = require('fs'),
     lsstream = require('ls-stream'),
     filter = require('stream-police'),
     split = require('split'),
@@ -25,14 +26,28 @@ var nopt = require('nopt'),
     input,
     options = nopt(noptions, shorts, process.argv)
 
+if (options.version) return version()
+if (options.help) return help()
+
 if (options.dir) {
   input = lsstream(path.resolve(options.dir))
 } else {
   input = process.stdin.pipe(split())
 }
 
+options.module = (options.module || []).concat(options.argv.remain)
+
 options.module.map(function (mod) {
   return /\//.test(mod) ? path.resolve(process.cwd(), mod) : mod
 })
 
 input.pipe(filter({ verify: [/\.js$/] })).pipe(jut(options)).pipe(process.stdout)
+
+function version() {
+  process.stdout.write('jut version ' + package.version + '\n')
+}
+
+function help() {
+  version()
+  fs.createReadStream(path.join(__dirname, '../help.txt')).pipe(process.stdout)
+}
